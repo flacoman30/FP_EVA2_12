83ea6ef5483c646c3205bbf4cc96e4bada3b540d
flacoman30 <L24550329@chihuahua2.tecnm.mx>
2024-11-06 13:37:52 -0600

    Add files via upload

---
+++ EVA2_12_TIPOS_DE_DATOS.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_12_tipos_de_datos;
+
+/**
+ *
+ * @author flac3
+ */
+public class EVA2_12_TIPOS_DE_DATOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        byte valor =-128;
+        byte valor2 = 127;
+        
+      System.out.println("valor ="+ (valor));
+      System.out.println("valor ="+ (valor2));
+      short val = -32768;
+      short val2 = 32767;
+      System.out.println("val ="+ (val));
+      System.out.println("val2 ="+ (val2));
+      int x = -2147483613;
+      int y = 2147483613;
+      
+      
+      
+    }
+    
+}
